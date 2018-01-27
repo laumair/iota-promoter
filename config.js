49a0e536b83967d707cca93bdf26053a51e3ad9e
Umair Sarfraz <aquadestructor@icloud.com>
2018-01-27 10:58:46 +0500

    Remove private nodes

--- config.js
+++ config.js
@@ -1,10 +1,7 @@
 module.exports = {
   NODES: [
-    'http://trinity.iota.fm:80',
     'http://iri2.iota.fm:80',
-    'https://www.veriti.io',
-    'http://148.251.181.105:14265',
-    'http://nodes.iota.fm:80',
+    'https://www.veriti.io'
   ],
   UNCONFIMED_BUNDLES_PATH: './bundles.list',
   FAILED_REATTACHS_PATH: './failed.list',
