93b81496d6bfbb8b37900c86df073118c018f656
Umair Sarfraz <aquadestructor@icloud.com>
2018-01-27 18:17:18 +0500

    Pass arguments to getConsistentTail, make sure we check for empty bundles/failed list before stopping

--- config.js
+++ config.js
@@ -1,7 +1,10 @@
 module.exports = {
   NODES: [
-    'http://iri2.iota.fm:80',
-    'https://www.veriti.io'
+      'http://trinity.iota.fm:80',
+      'http://iri2.iota.fm:80',
+      'https://www.veriti.io',
+      'http://148.251.181.105:14265',
+      'http://nodes.iota.fm:80'
   ],
   UNCONFIMED_BUNDLES_PATH: './bundles.list',
   FAILED_REATTACHS_PATH: './failed.list',
