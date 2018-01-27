f8eda411e0caec3e73c2acca94b186f55b5982db
Umair Sarfraz <aquadestructor@icloud.com>
2018-01-27 18:17:18 +0500

    Pass arguments to getConsistentTail, make sure we check for empty bundles/failed list before stopping

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const fs = require('fs');
 const config = require('./config');
 const { Promoter } = require('./promoter');
 const { getProvider, setupFiles, getExistingData } = require('./helpers');
