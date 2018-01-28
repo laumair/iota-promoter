924f46911388ea1581cf2cef58b00059c0766af5
Umair Sarfraz <aquadestructor@icloud.com>
2018-01-28 19:30:53 +0500

    Resolve linting

--- index.js
+++ index.js
@@ -1,68 +1,75 @@
 const config = require('./config');
 const { Promoter } = require('./promoter');
-const { getProvider, setupFiles, getExistingData } = require('./helpers');
+const {
+  getProvider,
+  setupFiles,
+  getExistingData,
+} = require('./helpers');
 
-const initialize = () => {
-    if (process.argv.length < 3) {
-        throw new Error('Argument missing. Run "--all" to start promoting all bundles. "--f" to start previously failed bundles.');
-    }
-
-    const arg = process.argv[2];
-
-    if (arg === '--all') {
-        return processList(true);
-    } else if (arg === '--f') {
-        return processList(false);
-    }
-
-    throw new Error('Invalid argument provided.');
-};
-
-
+/* eslint-disable arrow-body-style */
 const getPromotionArgs = () => {
-    return new Promise((resolve, reject) => {
-        const result = {
-            bundles: [],
-            failed: [],
-            confirmed: []
-        };
+  return new Promise((resolve, reject) => {
+    const result = {
+      bundles: [],
+      failed: [],
+      confirmed: [],
+    };
 
-        getExistingData(config.UNCONFIMED_BUNDLES_PATH)
-            .then(bundles => {
-                result.bundles = bundles.filter(bundle => bundle);
+    getExistingData(config.UNCONFIMED_BUNDLES_PATH)
+      .then(bundles => {
+        result.bundles = bundles.filter(bundle => bundle);
 
-                return getExistingData(config.FAILED_REATTACHS_PATH);
-            }).then(failed => {
-            result.failed = failed.filter(item => item);
+        return getExistingData(config.FAILED_REATTACHS_PATH);
+      }).then(failed => {
+        result.failed = failed.filter(item => item);
 
-            return getExistingData(config.CONFIRMED_PATH);
-        }).then(confirmed => {
-            result.confirmed = confirmed.filter(item => item);
-            resolve(result);
-        }).catch(err => reject(err));
-    });
+        return getExistingData(config.CONFIRMED_PATH);
+      }).then(confirmed => {
+        result.confirmed = confirmed.filter(item => item);
+        resolve(result);
+      })
+      .catch(err => reject(err));
+  });
 };
+
+/* eslint-enable arrow-body-style */
 
 const processList = promoteAll => {
-    setupFiles([
-        config.FAILED_REATTACHS_PATH,
-        config.CONFIRMED_PATH
-    ], () => getPromotionArgs()
-        .then(({ bundles, failed, confirmed }) => {
-            const node = getProvider(config.NODES);
+  setupFiles([
+    config.FAILED_REATTACHS_PATH,
+    config.CONFIRMED_PATH,
+  ], () => getPromotionArgs()
+    .then(({ bundles, failed, confirmed }) => {
+      const node = getProvider(config.NODES);
 
-            const promoter = new Promoter(
-                node,
-                bundles,
-                failed,
-                confirmed,
-                promoteAll
-            );
+      const promoter = new Promoter(
+        node,
+        bundles,
+        failed,
+        confirmed,
+        promoteAll,
+      );
 
-            return promoter.initialize();
-        }).catch(err => {
-            throw new Error(err);
-        }));
+      return promoter.initialize();
+    }).catch(err => {
+      throw new Error(err);
+    }));
+};
+
+const initialize = () => {
+  if (process.argv.length < 3) {
+    throw new Error('Argument missing. Run "--all" to start promoting all bundles. "--f" to start previously failed bundles.');
+  }
+
+  const arg = process.argv[2];
+
+  if (arg === '--all') {
+    return processList(true);
+  } else if (arg === '--f') {
+    return processList(false);
+  }
+
+  throw new Error('Invalid argument provided.');
 };
 
 initialize();
--- logger.js
+++ logger.js
@@ -1,10 +1,10 @@
 const chalk = require('chalk');
 
-const log = console.log;
+const { log } = console;
 
 module.exports = {
-    info: message => log(chalk.cyan(message)),
-    error: message => log(chalk.red(message)),
-    warn: message => log(chalk.yellow(message)),
-    success: message => log(chalk.green(message))
+  info: message => log(chalk.cyan(message)),
+  error: message => log(chalk.red(message)),
+  warn: message => log(chalk.yellow(message)),
+  success: message => log(chalk.green(message)),
 };
