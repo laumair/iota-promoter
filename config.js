63509bffe3c275e85ea7d2a264362a68cf9abbe1
Umair Sarfraz <aquadestructor@icloud.com>
2018-02-07 05:48:11 +0500

    Add default configuration

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  NODES: [
+    'http://localhost:14265'
+  ],
+  UNCONFIMED_BUNDLES_PATH: './bundles.list',
+  FAILED_REATTACHS_PATH: './failed.list',
+  CONFIRMED_PATH: './confirmed.list',
+};
